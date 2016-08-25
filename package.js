0027d2223a1f25b3e789b856f6a4e724ca3abf8b
Péter Pál Koszta <peterpal@fashionunited.com>
2016-08-25 17:35:01 +0200

    Bump version to 0.0.4

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'fuww:mysql-sync',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Synchronization of MySQL tables to mongodb collections',
   git: 'https://github.com/fuww/meteor-mysql-sync.git',
   documentation: 'README.md'
